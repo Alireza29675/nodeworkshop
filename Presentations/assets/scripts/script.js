403c8fdb09adfb1c82eb806826e457fc131cec22
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-27 21:21:38 +0330

    Very first slide has been made

---
+++ Presentations/assets/scripts/script.js
@@ -0,0 +1,6 @@
+/*
+    Javascript lines have comments for eagers
+*/
+
+var presentation = impress('root') // im using impress.js so i have to tell it which element should be initialize for our presentation
+presentation.init() // final call for launching the presentation
